28b5920a942be7b1c88bb4ff7abdc2b7266d1385
Phyo Than Htike <6531503173@lamduan.mfu.ac.th>
2024-09-30 21:55:43 +0700

    NewsPage

--- frontend/src/components/news/NewsItem.jsx
+++ frontend/src/components/news/NewsItem.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const NewsItem = (props) => {
   return (
-    <div className="item">
-        <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={props.image} alt=""/>
+    <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 ">
+        <div className='w-64 h-30'>
+        <img className="max-w-full h-auto object-contain rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={props.image} alt=""/>
+        </div>
         <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.title}</h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.featuredline}</p>
+            <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900">{props.title}</h5>
+            <p className="mb-3 text-sm text-gray-700">{props.featuredline}</p>
         </div>
     </div>
   )
--- frontend/src/pages/NewsDetail/NewsDetail.jsx
+++ frontend/src/pages/NewsDetail/NewsDetail.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const NewsDetail = () => {
+  const { id } = useParams();
+  // Fetch the product details by ID here or use the ID to get the product details from props/state/context
   return (
-    <div>NewsDetail</div>
-  )
+    <div className="product-detail">
+      <h1>Product Detail for ID: {id}</h1>
+      {/* Render the product details here */}
+    </div>
+  );
 }
 
 export default NewsDetail
